fd1971761b9da20b76c7db121e3726b4a90cae78
lupperant <75021896+lupperant@users.noreply.github.com>
2023-10-29 18:25:23 +0300

    Update task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -2,12 +2,11 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-num = 1
+num = 0
 
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-while money_capital - spend >= 0:
-    money_capital += salary
-    money_capital -= spend
+while money_capital + salary - spend >= 0:
+    money_capital += salary - spend
     spend *= 1 + increase
     num += 1
 
